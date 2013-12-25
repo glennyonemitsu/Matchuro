f4f2970f142edb83da64f0b12ec952d529bb92ed
Glenn Yonemitsu <glenn@krossover.com>
2013-12-25 14:01:24 -0500

    Initial Commit

---
+++ Matchuro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchuro
+//
+//  Created by Glenn Yonemitsu on 12/25/13.
+//  Copyright (c) 2013 Glenn Yonemitsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchuroTests/MatchuroTests.m
@@ -0,0 +1,34 @@
+//
+//  MatchuroTests.m
+//  MatchuroTests
+//
+//  Created by Glenn Yonemitsu on 12/25/13.
+//  Copyright (c) 2013 Glenn Yonemitsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MatchuroTests : XCTestCase
+
+@end
+
+@implementation MatchuroTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
